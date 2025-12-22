faae7c8d55506cae00196e5ddb37c02d0fc5313a
Kritika Koria <kritikakoria.600202@gmail.com>
2025-12-22 22:51:35 +0530

    Fix Navbar import case

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from '../components/Navbar'
+import {Navbar} from '../components/Navbar'
 import {useSelector} from 'react-redux'
 import Loader from '../components/Loader'
 import Login from './Login'
