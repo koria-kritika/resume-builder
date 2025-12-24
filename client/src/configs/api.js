f3a973bcbcb09582b4d1b2bf1bf79a872f1a4629
koria_kritika <165071937+koria-kritika@users.noreply.github.com>
2025-12-24 21:39:44 +0530

    Update api.js

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,7 +1,15 @@
-import axios from 'axios'
+import axios from "axios"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
+  baseURL: import.meta.env.VITE_BASE_URL,
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token")
+  if (token) {
+    config.headers.Authorization = token
+  }
+  return config
 })
 
 export default api
