d6067acc0ac9cf5bd0359fa4f3e92e9c775ca6c5
koria_kritika <165071937+koria-kritika@users.noreply.github.com>
2025-12-24 21:26:24 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { Lock, Mail, User2Icon } from 'lucide-react'
 import React from 'react'
+import { useNavigate } from "react-router-dom"
 import api from '../configs/api'
 import { useDispatch } from 'react-redux'
 import { login } from '../app/features/authSlice'
@@ -8,6 +9,7 @@ import toast from 'react-hot-toast'
 const Login = () => {
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
   const query = new URLSearchParams(window.location.search)
   const urlState = query.get('state')
   const [state, setState] = React.useState(urlState || "login")
@@ -21,12 +23,21 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const { data } = await api.post(`/api/users/${state}`, formData)
+     const payload =
+      state === "login"
+        ? {
+            email: formData.email,
+            password: formData.password,
+          }
+        : formData
+            const { data } = await api.post(`/api/users/${state}`,payload)
             dispatch(login(data))
             localStorage.setItem('token', data.token)
             toast.success(data.message)
+            navigate("/app")
         } catch (error) {
             toast(error?.response?.data?.message || error.message)
+             
         }
     }
 
