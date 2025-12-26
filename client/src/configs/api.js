af24ba7d69319197d49cc9b5416ca7d896b9ea80
koria_kritika <165071937+koria-kritika@users.noreply.github.com>
2025-12-26 23:40:40 +0530

    Update api.js

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,15 +1,15 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-})
+  baseURL: "https://resume-builder-backend-qv2x.onrender.com",
+});
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token")
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem("token");
   if (token) {
-    config.headers.Authorization = token
+    config.headers.Authorization = `Bearer ${token}`; 
   }
-  return config
-})
+  return config;
+});
 
-export default api
+export default api;
