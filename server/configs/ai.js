ea570db399cb135c2a73275c36cce48b8eb53a0d
koria_kritika <165071937+koria-kritika@users.noreply.github.com>
2025-12-27 22:06:19 +0530

    Update ai.js

--- server/configs/ai.js
+++ server/configs/ai.js
@@ -22,8 +22,9 @@
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 export async function geminiGenerate(prompt) {
-  const MODEL = "gemini-1.5-pro";
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/${MODEL}:generateContent?key=${GEMINI_API_KEY}`;
+
+  const url =    "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent" +
+    `?key=${GEMINI_API_KEY}`;
 
   const response = await fetch(url, {
     method: "POST",
