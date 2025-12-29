12c7a3c2e4c8e07405b454d393999120b5c3fe92
koria_kritika <165071937+koria-kritika@users.noreply.github.com>
2025-12-29 19:10:29 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,42 @@
+// import React, { useEffect } from 'react'
+// import { Route, Routes } from 'react-router-dom'
+// import Home from './pages/Home'
+// import Layout from './pages/Layout'
+// import Dashboard from './pages/Dashboard'
+// import ResumeBuilder from './pages/ResumeBuilder'
+// import Preview from './pages/Preview'
+// import Login from './pages/Login'
+// import { useDispatch } from 'react-redux'
+// import api from './configs/api'
+// import { login, setLoading } from './app/features/authSlice'
+// import {Toaster} from 'react-hot-toast'
+
+// const App = () => {
+
+//   const dispatch = useDispatch()
+
+//   const getUserData = async () => {
+//     const token = localStorage.getItem('token')
+//     try {
+//       if(token){
+//         const { data } = await api.get('/api/users/data', {headers: {Authorization: token}})
+//         if(data.user){
+//           dispatch(login({token, user: data.user}))
+//         }
+//         dispatch(setLoading(false))
+//       }else{
+//         dispatch(setLoading(false))
+//       }
+//     } catch (error) {
+//       dispatch(setLoading(false))
+//       console.log(error.message)
+//     }
+//   }
+
+//   useEffect(()=>{
+//     getUserData()
+//   },[])
+
 import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
@@ -9,7 +48,7 @@ import Login from './pages/Login'
 import { useDispatch } from 'react-redux'
 import api from './configs/api'
 import { login, setLoading } from './app/features/authSlice'
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
 
@@ -17,25 +56,51 @@ const App = () => {
 
   const getUserData = async () => {
     const token = localStorage.getItem('token')
+
+    if (!token) {
+      dispatch(setLoading(false))
+      return
+    }
+
     try {
-      if(token){
-        const { data } = await api.get('/api/users/data', {headers: {Authorization: token}})
-        if(data.user){
-          dispatch(login({token, user: data.user}))
-        }
-        dispatch(setLoading(false))
-      }else{
-        dispatch(setLoading(false))
+      // ❌ NO headers here
+      const { data } = await api.get('/api/users/data')
+
+      if (data?.user) {
+        dispatch(login({ token, user: data.user }))
       }
     } catch (error) {
+      console.error(error?.response?.data?.message || error.message)
+      localStorage.removeItem('token')
+    } finally {
       dispatch(setLoading(false))
-      console.log(error.message)
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getUserData()
-  },[])
+  }, [])
+
+  return (
+    <>
+      <Toaster position="top-right" />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route element={<Layout />}>
+          <Route path="/app/dashboard" element={<Dashboard />} />
+          <Route path="/app/builder/:id" element={<ResumeBuilder />} />
+          <Route path="/app/preview/:id" element={<Preview />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
+
 
   return (
     <>
